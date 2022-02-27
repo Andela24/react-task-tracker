771debdef2bf85c492693625cd59992708141124
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-27 17:01:34 -0500

    create db.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header({title, onAdd, showAdd}) {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color= {showAdd ? 'red' : 'green'}
+      <Button color= {showAdd ? 'red' : 'green'}  /**making dynamic btn*/
       text= {showAdd ? 'Close' : 'Add'} 
       onClick={onAdd}/>
     </header>
