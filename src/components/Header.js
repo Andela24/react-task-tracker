cd8e3a26009910760afdde3b5a5bc2ea39466c34
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-27 16:40:33 -0500

    toggle btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Button from './Button'
 
-function Header({title, onAdd}) {
+function Header({title, onAdd, showAdd}) {
 
 
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color= 'green' text= 'Add' onClick={onAdd}/>
+      <Button color= {showAdd ? 'red' : 'green'}
+      text= {showAdd ? 'Close' : 'Add'} 
+      onClick={onAdd}/>
     </header>
   )
 }
